abd9fa243630f917b4c912c94f847eb7888b6110
DianaMMartins <dian_marq_mart@hotmail.com>
2023-03-27 17:04:01 +0100

    commit working on connecting to mongodb

--- server/database.js
+++ server/database.js
@@ -1,35 +1,17 @@
+const { MongoClient } = require('mongodb')
 let dbConnection;
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY
 let uri = `mongodb+srv://pindernc:Xa7nqPHPaMKEEZDz@pinder.skvgszw.mongodb.net/?retryWrites=true&w=majority`;
-const express = require('express');
-const mongodb = require('mongodb');
-const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const test = await loadDemoCollection();
-    res.send(await test.find({}).toArray())
-});
-
-
-
-
-// async function main() {
-//   const uri =
-//     "mongodb+srv://pindernc:Xa7nqPHPaMKEEZDz@pinder.skvgszw.mongodb.net/?retryWrites=true&w=majority";
-//   const client = new MongoClient(uri);
-
-//   try {
-//     // Connect to the MongoDB cluster
-//     await client.connect();
-//     const db = client.db('sample_mflix');
-//     const collection = db.collection('comments');
-//     const first = await collection.findOne();
-//     console.log(first);
-//   } catch (e) {
-//     console.error(e);
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// main().catch(console.error);
+module.exports = { 
+    connectToDb: (cb) => {
+        MongoClient.connect(uri).then((client)=>{
+            dbConnection = client.db();
+            return cb();
+        }).catch((error) => {
+            console.log(error);
+            return cb(error);
+        })
+    },
+    getDb: () => dbConnection
+ }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+// const monk = require("monk");
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -9,10 +10,21 @@ const io = new Server(server, {
     origin: "http://localhost:3000",
   },
 });
+const db = require("monk")(
+  "mongodb+srv://pindernc:Xa7nqPHPaMKEEZDz@pinder.skvgszw.mongodb.net/?retryWrites=true&w=majority"
+//   'mongodb://pindernc:Xa7nqPHPaMKEEZDz@localhost:4000/sample_mflix?authSource=admin'
+);
+const collectionComments = db.get("comments");
 
 io.on("connection", (socket) => {
+    // const { collections } = collectionComments;
+    // console.log(collections);
   console.log(socket.id, "connected");
   socket.on('Hello', () => {
+    collectionComments.find({}).then(docs => {
+        console.log(docs);
+        console.log('i\'m in find one');
+    })
     console.log('hello');
     socket.emit('Bye');
   })
