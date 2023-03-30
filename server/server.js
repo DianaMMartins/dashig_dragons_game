781c087cfda2c1cd1587d08878aabe4dffb20efb
William Lee <Williamglee371@gmail.com>
2023-03-30 16:59:01 +0100

    commit started to work on projectile for damage

--- server/server.js
+++ server/server.js
@@ -5,70 +5,86 @@ const server = http.createServer(app);
 const cors = require("cors");
 const { Server } = require("socket.io");
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
+	cors: {
+		origin: "http://localhost:3000",
+	},
 });
 const mongoose = require("mongoose");
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const {
-  getEnemies,
-  getTowers,
-  getGoal,
-  getPlayer,
-  getGameScore,
-  postPlayerScore,
+	getEnemies,
+	getTowers,
+	getGoal,
+	getPlayer,
+	getGameScore,
+	postPlayerScore,
 } = require("./controllers/gameControllers");
 
 let playerIds = [];
 let players = [];
 
 io.on("connection", (socket) => {
-  console.log(socket.id, "connected");
+	console.log(socket.id, "connected");
 
-  if (playerIds.length < 2) {
-    playerIds.push(socket.id);
+	if (playerIds.length < 2) {
+		playerIds.push(socket.id);
 
-    socket.emit("assignId", socket.id);
-  }
+		socket.emit("assignId", socket.id);
+	}
 
-  if (playerIds.length === 2) {
-    io.emit("sendAllIds", playerIds);
-    getPlayer().then((playerData) => {
-      const playerTemplate = {
-        location: { y: 0 },
-        health: playerData[0].health,
-        coins: playerData[0].coins,
-        weapon: playerData[0].weapon,
-      };
+	if (playerIds.length === 2) {
+		io.emit("sendAllIds", playerIds);
+		getPlayer().then((playerData) => {
+			const playerTemplate = {
+				location: { y: 0 },
+				health: playerData[0].health,
+				coins: playerData[0].coins,
+				weapon: playerData[0].weapon,
+			};
 
-      players = [{ ...playerTemplate }, { ...playerTemplate }];
-    });
-  }
+			players = [{ ...playerTemplate }, { ...playerTemplate }];
+		});
+	}
 
-  socket.on("updatePlayerOnePosition", (data) => {
-    players[0].location.y = data;
-    socket.broadcast.emit("updatePlayerOnePosition", players[0].location);
+	socket.on("updatePlayerOnePosition", (data, direction) => {
+		players[0].location.y = data;
+		socket.broadcast.emit(
+			"updatePlayerOnePosition",
+			players[0].location,
+			direction
+		);
+	});
+
+	socket.on("updatePlayerTwoPosition", (data, direction) => {
+		players[1].location.y = data;
+		socket.broadcast.emit(
+			"updatePlayerTwoPosition",
+			players[1].location,
+			direction
+		);
+	});
+
+	socket.on("player1shot",()=>{
+    io.emit("player1shot")
   });
 
-  socket.on("updatePlayerTwoPosition", (data) => {
-    players[1].location.y = data;
-    socket.broadcast.emit("updatePlayerTwoPosition", players[1].location);
-  });
+  socket.on("player2shot",()=>{
+    io.emit("player2shot")
+  })
 
-  socket.on("disconnect", () => {
-    console.log("disconnect");
-  });
+	socket.on("disconnect", () => {
+		console.log("disconnect");
+	});
 });
 
 const db = mongoose
-  .connect(
-    "mongodb+srv://newuser:zaKUwAsSSChyUO3U@pinder.skvgszw.mongodb.net/Game"
-  )
-  .then(() => {
-    server.listen(4040);
-  });
+	.connect(
+		"mongodb+srv://newuser:zaKUwAsSSChyUO3U@pinder.skvgszw.mongodb.net/Game"
+	)
+	.then(() => {
+		server.listen(4040);
+	});
 
 module.exports = { server, io, app };
